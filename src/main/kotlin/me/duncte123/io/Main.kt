09a86ec7dea9bbd2432ca846ed051b9fb6cef70e
duncte123 <contact@duncte123.me>
2024-07-09 08:37:12 +0200

    SOURCE CODE IN BIO

--- src/main/kotlin/me/duncte123/io/Main.kt
+++ src/main/kotlin/me/duncte123/io/Main.kt
@@ -1,13 +1,34 @@
 package me.duncte123.io
 
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 val commandManager = CommandManager()
 
 fun main() {
     commandManager.registerUserCommands()
 
-    JDABuilder.createLight(System.getenv("BOT_TOKEN"))
+    val jda = JDABuilder.createLight(System.getenv("BOT_TOKEN"))
         .addEventListeners(Listener())
+        .setActivity(Activity.customStatus("SOURCE CODE IN BIO"))
         .build()
+
+    val timer = Executors.newSingleThreadScheduledExecutor {
+        Thread(it).apply {
+            name = "Status updater"
+            isDaemon = true
+        }
+    }
+
+    timer.scheduleAtFixedRate(
+        {
+            jda.presence.setPresence(
+                Activity.customStatus("SOURCE CODE IN BIO"),
+                false
+            )
+        },
+        1L, 1L, TimeUnit.DAYS
+    )
 }
